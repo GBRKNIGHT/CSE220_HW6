81b39cec075fd99b80868b410ddab7b786307a60
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-03-19 00:50:06 +0000

    ?

--- src/hw6.c
+++ src/hw6.c
@@ -2,25 +2,28 @@
 
 int main(int argc, char *argv[]) {
     unsigned int argv_size = sizeof(argv);
+    char string_array[7][99];        
+    // try to split the line by space
     // because 0 stores the name of executable, we start from 1. s
     for(unsigned int i = 1; i < argv_size; i++){
-        char *line = argv[i];
-
+        char *line = &argv[i];
         // try to split the line by space
-        char string_array[7][99];
         int space_counter = 0;
-        while(*line != '\n')
+        char* line_pointer = &line[i];
+        while(*line_pointer != '\n')
         {
-            if(*line == ' '){
-                *line += 1;
+            if(*line_pointer == ' '){
+                line_pointer &= 
                 space_counter ++;
                 continue;
             }
             int j = 0;
             string_array[space_counter][j] = *line;
-            *line += 1;
+            line_pointer += 1;
         }
-        
+        if(space_counter < 7){
+            return 1;
+        }
     }
     return 0;
 }
