992e3ddd96093a16b39b4e7c73770a18d707852f
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-03-19 00:58:58 +0000

    recover

--- src/hw6.c
+++ src/hw6.c
@@ -2,28 +2,41 @@
 
 int main(int argc, char *argv[]) {
     unsigned int argv_size = sizeof(argv);
-    char string_array[7][99];        
-    // try to split the line by space
-    // because 0 stores the name of executable, we start from 1. s
-    for(unsigned int i = 1; i < argv_size; i++){
-        char *line = &argv[i];
-        // try to split the line by space
-        int space_counter = 0;
-        char* line_pointer = &line[i];
-        while(*line_pointer != '\n')
-        {
-            if(*line_pointer == ' '){
-                line_pointer &= 
-                space_counter ++;
-                continue;
+    for(int i = 0; i < argv_size;i++){
+        char* line = &argv[i];
+        int length = strlen(line);
+        int num_space = 0;
+        for(int j = 0; j < length; j++){
+            if(line[j] == ' '){
+                num_space ++;
             }
-            int j = 0;
-            string_array[space_counter][j] = *line;
-            line_pointer += 1;
         }
-        if(space_counter < 7){
+        if(num_space < 7){
             return 1;
         }
     }
+    // char string_array[7][99];        
+    // // try to split the line by space
+    // // because 0 stores the name of executable, we start from 1. s
+    // for(unsigned int i = 1; i < argv_size; i++){
+    //     char *line = &argv[i];
+    //     // try to split the line by space
+    //     int space_counter = 0;
+    //     char* line_pointer = &line[i];
+    //     while(*line_pointer != '\n')
+    //     {
+    //         if(*line_pointer == ' '){
+    //             line_pointer &= 
+    //             space_counter ++;
+    //             continue;
+    //         }
+    //         int j = 0;
+    //         string_array[space_counter][j] = *line;
+    //         line_pointer += 1;
+    //     }
+    //     if(space_counter < 7){
+    //         return 1;
+    //     }
+    // }
     return 0;
 }
