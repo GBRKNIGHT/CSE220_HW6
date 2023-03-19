8c214543b4e9e64aba037d68087210a9b5dc9de6
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-03-19 01:31:20 +0000

    find length of each substring

--- src/hw6.c
+++ src/hw6.c
@@ -2,41 +2,34 @@
 
 int main(int argc, char *argv[]) {
     unsigned int argv_size = sizeof(argv);
+    // if(argv_size < 7) {
+    //     return 1;
+    // }
     for(int i = 0; i < argv_size;i++){
         char* line = &argv[i];
+        if (line == NULL) return 1;
         int length = strlen(line);
         int num_space = 0;
+        int space_positions[7] = {-999, -999, -999, -999, -999, -999, -999};
         for(int j = 0; j < length; j++){
             if(line[j] == ' '){
+                space_positions[num_space] = j;
                 num_space ++;
             }
         }
-        if(num_space < 7){
+        if(num_space < 7) {
             return 1;
         }
+        // split by found space indexs
+        // find the length of each substrings. 
+        int string_lengths[7] = {-555, -555, -555, -555, -555, -555, -555};
+        for (int j = 0; j < sizeof(space_positions); j++){
+            string_lengths[j] = space_positions[j+1] - space_positions[j] - j;
+        }
+        
+        // int one_l = space_positions[1] - space_positions[0];// find length
+        // int two_l = space_positions[2] - space_positions[1] - 1; // minus one after each iteration 
+        // int three_l = space_positions[3] - space_positions[2] - 2;
     }
-    // char string_array[7][99];        
-    // // try to split the line by space
-    // // because 0 stores the name of executable, we start from 1. s
-    // for(unsigned int i = 1; i < argv_size; i++){
-    //     char *line = &argv[i];
-    //     // try to split the line by space
-    //     int space_counter = 0;
-    //     char* line_pointer = &line[i];
-    //     while(*line_pointer != '\n')
-    //     {
-    //         if(*line_pointer == ' '){
-    //             line_pointer &= 
-    //             space_counter ++;
-    //             continue;
-    //         }
-    //         int j = 0;
-    //         string_array[space_counter][j] = *line;
-    //         line_pointer += 1;
-    //     }
-    //     if(space_counter < 7){
-    //         return 1;
-    //     }
-    // }
     return 0;
 }
